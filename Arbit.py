c54fd9069f81d1c9b48e28da6eee5720b445791d
esauvisky <esauvisky@gmail.com>
2018-02-26 19:22:20 -0300

    Revert back to notifications all the time

--- Arbit.py
+++ Arbit.py
@@ -16,7 +16,6 @@ gi.require_version('Notify', '0.7')
 from gi.repository import Notify
 Notify.init('Arbit')
 
-
 ## Global Constants
 feesData = {'MBT': {'name': 'Mercado Bitcoin', 'color': 'e6194b', 'url': 'https://www.mercadobitcoin.com.br', 'url_book': 'https://www.mercadobitcoin.com.br/BRLBTC/negociacoes/', 'fees': {'in_BRL': [0.0199, 2.9], 'in_BTC': [0, 0], 'out_BRL': [0.0199, 2.9], 'out_BTC': [0, 0], 'trade_book': [0.003, 0], 'trade_market': [0.007, 0]}}, 'B2U': {'name': 'BitcoinToYou', 'color': '0082c8', 'url': 'https://www.bitcointoyou.com', 'url_book': 'https://broker.bitcointoyou.com/Negociacoes/externo', 'fees': {'in_BRL': [0.0189, 0], 'in_BTC': [0, 0], 'out_BRL': [0.0189, 0], 'out_BTC': [0, 0], 'trade_book': [0.0025, 0], 'trade_market': [0.006, 0]}}, 'BAS': {'name': 'Basebit', 'color': 'aaffc3', 'url': 'https://www.basebit.com.br', 'url_book': 'https://www.basebit.com.br', 'fees': {'in_BRL': [0.0149, 0], 'in_BTC': [0, 0], 'out_BRL': [0.0149, 0], 'out_BTC': [0, 0], 'trade_book': [0.0025, 0], 'trade_market': [0.006, 0]}}, 'FOX': {'name': 'FoxBit', 'color': 'f58231', 'url': 'http://foxbit.com.br/', 'url_book': 'https://foxbit.exchange/#market', 'fees': {'in_BRL': [0.0, 0], 'in_BTC': [0, 0], 'out_BRL': [0.0139, 0], 'out_BTC': [0, 0.0002], 'trade_book': [0.0025, 0], 'trade_market': [0.005, 0]}}, 'BIV': {'name': 'Bitinvest', 'color': 'e6beff', 'url': 'https://www.bitinvest.com.br', 'url_book': 'https://www.bitinvest.com.br/exchange/orders/negotiations', 'fees': {'in_BRL': [0, 0], 'in_BTC': [0, 0], 'out_BRL': [0.0099, 0], 'out_BTC': [0, 0], 'trade_book': [0.003, 0], 'trade_market': [0.003, 0]}}, 'FLW': {'name': 'flowBTC', 'color': '808080', 'url': 'https://trader.flowbtc.com', 'url_book': 'https://trader.flowbtc.com', 'fees': {'in_BRL': [0.005, 0], 'in_BTC': [0, 0], 'out_BRL': [0.0119, 8], 'out_BTC': [0, 0.0015], 'trade_book': [0.0035, 0], 'trade_market': [0.0035, 0]}}, 'NEG': {'name': 'Negocie Coins', 'color': 'd2f53c', 'url': 'https://www.negociecoins.com.br', 'url_book': 'http://www.negociecoins.com.br/negociacoes', 'fees': {'in_BRL': [0, 0], 'in_BTC': [0, 0], 'out_BRL': [0.009, 8.9], 'out_BTC': [0, 0.001], 'trade_book': [0.003, 0], 'trade_market': [0.004, 0]}}, 'LOC': {'name': 'LocalBitcoins', 'color': '911eb4', 'url': 'https://localbitcoins.com/', 'url_book': 'https://localbitcoins.com/', 'fees': {'in_BRL': [0, 0], 'in_BTC': [0, 0], 'out_BRL': [0, 0], 'out_BTC': [0, 0], 'trade_book': [0.01, 0], 'trade_market': [0, 0]}}, 'ARN': {'name': 'Arena Bitcoin', 'color': 'ffd8b1', 'url': 'http://www.arenabitcoin.com.br/', 'url_book': 'https://www.arenabitcoin.com/markets/btccny', 'fees': {'in_BRL': [0, 0], 'in_BTC': [0, 0], 'out_BRL': [0.001, 0], 'out_BTC': [0, 0.0005], 'trade_book': [0.0015, 0], 'trade_market': [0.0015, 0]}}, 'PAX': {'name': 'Paxful', 'color': 'ffe119', 'url': 'https://paxful.com/', 'url_book': 'https://paxful.com/buy-bitcoin', 'fees': {'in_BRL': [0, 0], 'in_BTC': [0, 0], 'out_BRL': [0, 0], 'out_BTC': [0, 0], 'trade_book': [0.01, 0], 'trade_market': [0, 0]}}, 'BSQ': {'name': 'Bitsquare', 'color': '800000', 'url': 'https://bitsquare.io/', 'url_book': 'https://market.bitsquare.io/?market=btc_brl', 'fees': {'in_BRL': [0, 0], 'in_BTC': [0, 0], 'out_BRL': [0, 0], 'out_BTC': [0, 0], 'trade_book': [0, 0.0005], 'trade_market': [0, 0.001]}}, 'BTD': {'name': 'BitcoinTrade', 'color': '000000', 'url': 'https://bitcointrade.com.br/', 'url_book': 'https://bitcointrade.com.br/marketplace', 'fees': {'in_BRL': [0, 0], 'in_BTC': [0, 0], 'out_BRL': [0.0099, 4.9], 'out_BTC': [0, 0], 'trade_book': [0.005, 0], 'trade_market': [0.005, 0]}}, 'BZX': {'name': 'Braziliex', 'color': '3cb44b', 'url': 'https://braziliex.com/', 'url_book': 'https://braziliex.com', 'fees': {'in_BRL': [0, 0], 'in_BTC': [0, 0], 'out_BRL': [0.0025, 9], 'out_BTC': [0, 0.001], 'trade_book': [0.005, 0], 'trade_market': [0.005, 0]}}}
 orderBookStatsURL = 'https://api.bitvalor.com/v1/order_book_stats.json'
@@ -37,9 +36,9 @@ def fetch_data():
             else:
                 print('[ERROR] Got bad request status code: ' + response.status_code)
         except Exception as e:
-            print('[ERROR] Something happened while fetching the order book: \n' + e)
+            print('[ERROR] Something bad happened while fetching the order book... Maybe connection error?')
 
-        print('Trying again in ' + (2 ** attempt) + ' seconds...')
+        print('Trying again in ' + str(2 ** attempt) + ' seconds...')
         time.sleep(2 ** attempt)
         attempt += 1
 
@@ -80,14 +79,14 @@ def main():
 
                 # TODO: Notificates only for foxbit as selling exchange to debug
                 # In the future it should alternate between exchanges
-                if sExchange == 'FOX':
-                    notification = Notify.Notification.new('Arbitrage Opportunity (' + str(profit) + '%)',
-                                                           'SELL in\t' + sExchange + '\tat\t' + str(sPrice['bid']) + '\t(you will receive \t' + str(round(totalSPrice)) + ')\n' +
-                                                           'BUY  in\t' + bExchange + '\tat\t' + str(bPrice['ask']) + '\t(you will pay \t\t' + str(round(totalBPrice)) + ')',
-                                                           current_dir + '/profit.png')
-                    notification.set_urgency(2)
-                    notification.show()
-                    os.system('paplay beep.wav')
+                # if sExchange == 'FOX':
+                notification = Notify.Notification.new('Arbitrage Opportunity (' + str(profit) + '%)',
+                                                       'SELL in\t' + sExchange + '\tat\t' + str(format(sPrice['bid'], '.2f')) + '\t(you will receive \t' + str(round(totalSPrice)) + ')\n' +
+                                                       'BUY  in\t' + bExchange + '\tat\t' + str(format(bPrice['ask'], '.2f')) + '\t(you will pay     \t' + str(round(totalBPrice)) + ')',
+                                                       current_dir + '/profit.png')
+                notification.set_urgency(2)
+                notification.show()
+                os.system('paplay beep.wav')
 
 
 ## The Loop
